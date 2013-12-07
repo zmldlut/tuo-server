c951d39014677797255382aad8f445fed904bf66
zml <zml@zml-PC>
2013-12-07 15:09:12 +0800

    冲突

---
+++ lib/Demos/App/Server/FriendsServer.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * Demos App
+ *
+ * @category   Demos
+ * @package    Demos_App_Server
+ * @author     James.Huang <huangjuanshi@163.com>
+ * @license    http://www.apache.org/licenses/LICENSE-2.0
+ * @version    $Id$
+ */
+
+require_once 'Demos/App/Server.php';
+
+/**
+ * @package Demos_App_Server
+ */
+class FriendsServer extends Demos_App_Server
+{
+	/**
+	 * ---------------------------------------------------------------------------------------------
+	 * > 全局设置：
+	 * <code>
+	 * </code>
+	 * ---------------------------------------------------------------------------------------------
+	 */
+	public function __init ()
+	{
+		parent::__init();
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	// service api methods
+	/**
+	 * ---------------------------------------------------------------------------------------------
+	 * > 接口说明：获取好友列表
+	 * <code>
+	 * URL地址：/Friends/fansList
+	 * 提交方式：GET
+	 * </code>
+	 * ---------------------------------------------------------------------------------------------
+	 * @title 获取好友列表
+	 * @action /Friends/fansList
+	 * @method get
+	 */
+	public function fansListAction ()
+	{
+		$this -> doAuth();
+		$relationDao = $this->dao->load('Core_Relationship');
+		$fanslist = $relationDao -> getFansList($this->user['id']);
+		if(!$fanslist){
+			$this ->render('15001','Get fans list failed');
+		}
+		$this->render('10000', 'Get fans list ok', array(
+				'Fans.list' => $fanslist
+		));
+	}
+	/**
+	 * ---------------------------------------------------------------------------------------------
+	 * > 接口说明：搜索好友接口
+	 * <code>
+	 * URL地址：/Friends/search
+	 * 提交方式：POST
+	 * </code>
+	 * ---------------------------------------------------------------------------------------------
+	 * @title 搜索好友接口
+	 * @action /Friends/search
+	 * @params name james STRING
+	 * @method get
+	 */
+	public function searchAction ()
+	{
+		$this->doAuth();
+		
+		$name = $this->param('name');
+		// get extra user info
+		$friends = $this->dao->load('Core_User');
+		
+        $friend = $friends->getByName($name);
+		if($friend){
+			$this->render('10000', 'Search friend ok!', array(
+				'friend' => $friend
+			));
+		}
+		$this->render('14013', 'Search friend failed!');
+	}
+	
+	/**
+	 * ---------------------------------------------------------------------------------------------
+	 * > 接口说明：添加好友接口
+	 * <code>
+	 * URL地址：/Friends/add
+	 * 提交方式：POST
+	 * </code>
+	 * ---------------------------------------------------------------------------------------------
+	 * @title 添加好友接口
+	 * @action /Friends/add
+	 * @params userid 1 INT
+	 * @method get
+	 */
+	public function addAction ()
+	{
+		$this->doAuth();
+	
+		$userid = $this->param('userid');
+		// get extra user info
+		$relationship = $this->dao->load('Core_Relationship');
+// 		$friend = $relationship->getById($id);
+		if(!$relationship->existRelationShip($this->user['id'], $userid)){
+			
+			$relationship->create(array(
+				'userid' => $this->user[id],
+				'fansid' => $userid,
+			));
+			$this->render('10000', 'Add friend ok!');
+		}
+		else 
+		{
+			$this->render('10009', '该用户已经是您的好友！');
+		}
+		$this->render('14013', 'Add friend failed!');
+	}
+	
+	/**
+	 * ---------------------------------------------------------------------------------------------
+	 * > 接口说明：删除好友接口
+	 * <code>
+	 * URL地址：/Friends/del
+	 * 提交方式：POST
+	 * </code>
+	 * ---------------------------------------------------------------------------------------------
+	 * @title 删除好友接口
+	 * @action /Friends/del
+	 * @params userid 1 INT
+	 * @method get
+	 */
+	public function delAction ()
+	{
+		$this->doAuth();
+	
+		$userid = $this->param('userid');
+		// get extra user info
+		$relationship = $this->dao->load('Core_Relationship');
+		// 		$friend = $relationship->getById($id);
+		if($relationship->existRelationShip($this->user['id'], $userid)){
+
+			$relationship->deleteRelationShip($this->user['id'],$userid);
+			$this->render('10000', 'Del friend ok!');
+		}
+		else
+		{
+			$this->render('10009', '用户不是您的好友！');
+		}
+		$this->render('14013', 'Del friend failed!');
+	}
+}
---
+++ lib/Demos/App/Server/MicroblogServer.php
@@ -0,0 +1,162 @@
+<?php
+
+
+require_once 'Demos/App/Server.php';
+
+/**
+ * @package Demos_App_Server
+ */
+class MicroblogServer extends Demos_App_Server
+{
+	/**
+	 * ---------------------------------------------------------------------------------------------
+	 * > 全局设置：
+	 * <code>
+	 * </code>
+	 * ---------------------------------------------------------------------------------------------
+	 */
+	public function __init ()
+	{
+		parent::__init();
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	// service api methods
+	/**
+	 * ---------------------------------------------------------------------------------------------
+	 * > 接口说明：获取好友说说列表 （包括自己的和好友的）
+	 * <code>
+	 * URL地址：/Microblog/blogList
+	 * 提交方式：GET
+	 * 参数#1：typeId，类型：INT，必须：YES
+	 * 参数#2：pageId，类型：INT，必须：YES
+	 * </code>
+	 * ---------------------------------------------------------------------------------------------
+	 * @title 好友说说列表接口
+	 * @action /microblog/blogList
+	 * @params pageId 0 INT
+
+	 * @method get
+	 */
+	public function blogListAction ()
+	{
+		$this->doAuth();
+	
+		$pageId = intval($this->param('pageId'));
+		$blogList = array();
+		$blogDao = $this->dao->load('Core_Microblog');
+		$blogList = $blogDao->getFansListByUser($this->user['id'], $pageId);
+		if ($blogList) {
+			$this->render('10000', 'Get blog list ok', array(
+					'Microblog.list' => $blogList
+			));
+		}
+		$this->render('14008', 'Get blog list failed');
+	}
+	
+	
+	/**
+	 * ---------------------------------------------------------------------------------------------
+	 * > 接口说明：获取用户说说列表 （包括自己或好友的）
+	 * <code>
+	 * URL地址：/Microblog/userBlogList
+	 * 提交方式：GET
+	 * 参数#1：typeId，类型：INT，必须：YES
+	 * 参数#2：pageId，类型：INT，必须：YES
+	 * </code>
+	 * ---------------------------------------------------------------------------------------------
+	 * @title 用户说说列表接口
+	 * @action /microblog/userBlogList
+	 * @params userId 1 INT
+	 * @params pageId 0 INT
+	
+	 * @method get
+	 */
+	public function userBlogListAction ()
+	{
+		$this->doAuth();
+	
+		$userId = intval($this->param('userId'));
+		$pageId = intval($this->param('pageId'));
+	
+		$blogList = array();
+		$blogDao = $this->dao->load('Core_Microblog');
+		$blogList = $blogDao->getOwnListByUser($userId, $pageId);
+		if ($blogList) {
+			$this->render('10000', 'Get blog list ok', array(
+					'Microblog.list' => $blogList
+			));
+		}
+		$this->render('14008', 'Get blog list failed');
+	}
+
+
+	
+	/**
+	 * ---------------------------------------------------------------------------------------------
+	 * > 接口说明：用户签到接口
+	 * <code>
+	 * URL地址：/microblog/checkin
+	 * 提交方式：POST
+	 * 参数#1：被踩用户id，类型：INT，必须：YES
+	 * </code>
+	 * ---------------------------------------------------------------------------------------------
+	 * @title 用户签到接口
+	 * @action /microblog/checkin
+	 * @method post
+	 */
+	public function checkinAction ()
+	{
+		$this->doAuth();
+		
+		// 加分
+		$userDao = $this->dao->load('Core_User');
+		$userDao -> addScore($this->user['id'] , 1);
+		
+		// 发表说说
+		$blogDao = $this->dao->load('Core_Microblog');
+		$content = $this->user['name']." 今日已签到,成功获得1积分";
+		$blogDao->create(array(
+				'userid'	=> $this->user['id'],
+				'content'	=> $content,
+		));
+		$this->render('10000','Check in ok',$content);
+	}
+	
+	/**
+	 * ---------------------------------------------------------------------------------------------
+	 * > 接口说明：踩一脚接口
+	 * <code>
+	 * URL地址：/microblog/stamp
+	 * 提交方式：POST
+	 * 参数#1：stampon，类型：STRING，必须：YES
+	 * </code>
+	 * ---------------------------------------------------------------------------------------------
+	 * @title 踩一脚接口
+	 * @action /microblog/stamp
+	 * @params stamponId '' STRING
+	 * @method post
+	 */
+	public function stampAction ()
+	{
+		$this->doAuth();
+		$stamponId = intval($this->param("stamponId"));
+		// 加减分
+		$userDao = $this->dao->load('Core_User');
+		$userDao -> addScore($this->user['id'] , 1);
+		$userDao -> addScore($stamponId , -1);
+		
+		// 发表说说
+		$blogDao = $this->dao->load('Core_Microblog');
+		$content = $this->user['name']." 刚刚踩了 ".$userDao->getNameById($stamponId)." 一脚，成功偷得1积分";
+		$blogDao->create(array(
+				'userid'	=> $this->user['id'],
+				'content'	=> $content
+		));
+		
+		// 推送通知
+		
+		
+		$this->render('10000','stamp friends ok',$content);
+	}
+}
